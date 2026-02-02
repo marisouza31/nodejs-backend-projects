1f1a72cf8158962d9fefa2c752509472ac844630
Mariana Ocireu <99839123+marisouza31@users.noreply.github.com>
2026-02-02 19:40:59 -0300

    Delete src directory

--- src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const { createUser, listUsers } = require("../controllers/user.controller");
-
-const router = Router();
-
-router.post("/", createUser);
-router.get("/", listUsers); 
-
-module.exports = router;
